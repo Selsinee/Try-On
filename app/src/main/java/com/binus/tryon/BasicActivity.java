6ee3080a61f86745bdbae43e84a21fe3f5b4f8b1
Seline <seline@binus.edu>
2022-05-17 19:33:31 +0700

    Initial Commit

---
+++ app/src/main/java/com/binus/tryon/BasicActivity.java
@@ -0,0 +1,19 @@
+package com.binus.tryon;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import ai.deepar.deepar_example.R;
+
+public class BasicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
